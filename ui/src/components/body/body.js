d7d4a7f146605d6b4d75e382c15e1ff0d502306d
João Stinglin Neto <joao.neto@criticalsoftware.com>
2020-08-12 21:44:18 +0100

    project configs and header component

---
+++ ui/src/components/body/body.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './body.scss';
+
+import Header from '../header/header';
+
+function Body() {
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+}
+
+export default Body;
---
+++ ui/src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.scss';
+
+function Header() {
+    return (
+        <div className="app-header">
+           <div className="header-title">Breathtaking beauty</div>
+        </div>
+    );
+}
+
+export default Header;
