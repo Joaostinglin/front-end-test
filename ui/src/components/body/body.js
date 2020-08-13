1ca5ea34fe4ae8044ff0ec605245b72ea5e34043
João Stinglin Neto <joao.neto@criticalsoftware.com>
2020-08-13 10:09:47 +0100

    Video component created

--- ui/src/components/body/body.js
+++ ui/src/components/body/body.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './body.scss';
 
 import Header from '../header/header';
+import Video from '../video/video';
 
 function Body() {
     return (
         <div>
             <Header />
+            <Video />
         </div>
     );
 }
---
+++ ui/src/components/video/video.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './video.scss';
+
+function Video() {
+    return (
+        <div>
+           <div>Video component</div>
+        </div>
+    );
+}
+
+export default Video;
